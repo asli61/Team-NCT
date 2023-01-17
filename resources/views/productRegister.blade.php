868607cfbaafb9d6a89883b7a7fe7d6a3c31e361
asim61 <asimersoy2005@gmail.com>
2023-01-17 19:21:07 +0300

    frontedn commit

--- resources/views/productRegister.blade.php
+++ resources/views/productRegister.blade.php
@@ -1,22 +1,25 @@
-@extends('layouts.master')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>Ürün kayıt et</div> 
 
-@section('title', 'Login')
-
-@section('content')
-
-Ürün kayıt et
-
-<form method="post">
-    @csrf
-    <input type = "text" name = "serial_number" placeholder = "Ürün seri numarasını giriniz" >
-    <input type = "text" name = "adress" placeholder ="Ürünün yerleştirileceği yerin açık adresi" >
-    <button type = "submit">Kayıt Et</button>
- </form>
-
-@if (session()->get("error"))
+    <form method="post">
+        @csrf
+        <input type = "text" name = "serial_number" placeholder = "Ürün seri numarasını giriniz" >
+        <input type = "text" name = "adress" placeholder ="Ürünün yerleştirileceği yerin açık adresi" >
+        <button type = "submit">Kayıt Et</button>
+     </form>
     
-{{session()->get("flash")}}
-    
-@endif
-
-@endsection
+    @if (session()->get("error"))
+        
+    {{session()->get("flash")}}
+        
+    @endif
+</body>
+</html>
