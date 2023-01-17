716394cba7298959cfe5e3b092270f6065c27b58
AliErsoy <aliersoytr35@gmail.com>
2023-01-17 22:24:42 +0300

    labels readded

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,30 @@
 <div class="container">
-<div class="email-styling">
-<input id="email" type="email" name="email" required autocomplete="email">
-</div>
-<div class="password-styling">
-<input id="password" type="password"  name="password" required autocomplete="current-password">
-</div>
-<div class="remember-styling">
-<input id="remember" type="checkbox" name="remember"  {{ old('remember') ? 'checked' : '' }}>
-</div>
-<button type="submit" class="login-button">
-    {{ __('Login') }}
-</button>
-<div class="forgot-password">
-@if (Route::has('password.request'))
+    <div class="email-styling">
+        <label for="email">{{ __('Email Address') }}</label>
+        <input id="email" type="email" name="email" required autocomplete="email">
+    </div>
+
+    <div class="password-styling">
+        <label for="password">{{ __('Password') }}</label>
+        <input id="password" type="password"  name="password" required autocomplete="current-password">
+    </div>
+
+    <div class="remember-styling">
+        <input id="remember" type="checkbox" name="remember"  {{ old('remember') ? 'checked' : '' }}>
+        <label class="form-check-label" for="remember">
+            {{ __('Remember Me') }}
+        </label>
+    </div>
+
+    <button type="submit" class="login-button">
+        {{ __('Login') }}
+    </button>
+
+    <div class="forgot-password">
+        @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
-</div>
+        @endif
+    </div>
 </div>
