b824a454db200f69288582ec16c4f91fa408837e
asim61 <asimersoy2005@gmail.com>
2023-01-17 22:25:50 +0300

    migration error fix try

--- database/migrations/2023_01_17_183018_create_product_infos_table.php
+++ database/migrations/2023_01_17_183018_create_product_infos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('product_infos', function (Blueprint $table) {
-            $table->id();
+            $table->id("product-id");
             $table->string("serial_number");
             $table->string("adress")->nullable();
             $table->timestamps();
--- database/migrations/2023_01_18_072838_fire_data.php
+++ database/migrations/2023_01_18_072838_fire_data.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('FireTable', function (Blueprint $table) {
-            $table->id();
+            $table->id("fire_id");
             $table->foreign('product_id')
             ->references('id')->on('product_infos')->onDelete('cascade')->nullable();
             $table->timestamps();
