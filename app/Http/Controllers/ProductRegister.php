b87cd64b63e40f972ee551d4c8ce29d0309b0cee
asim61 <asimersoy2005@gmail.com>
2023-01-18 10:13:35 +0300

    product register controller made

--- app/Http/Controllers/ProductRegister.php
+++ app/Http/Controllers/ProductRegister.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\FireTableInput;
+use App\Models\ProductInfo;
 use Illuminate\Http\Request;
 
 class ProductRegister extends Controller
@@ -19,17 +20,22 @@ class ProductRegister extends Controller
         
         if((!empty($request["serial_number"])) & (!empty($request["adress"])))
         {
-            $product = FireTableInput::where([
+            $product = ProductInfo::where([
                 "serial_number" => $request["serial_number"]])->first();
         }
 
         if(!empty($product["id"]))
         {
             FireTableInput::create([
-                'adress' => $request["adress"],
+                'product_id' => $request["id"],
                 'created_at' => now(),
                 'updated_at' => now()
             ]);
+        }
+
+        else
+        {
+            redirect(route("product-register"))->with(["error" => true , "flash" => "Either serial number doesn't exist or adress is empty"]);
         }
         
     }
