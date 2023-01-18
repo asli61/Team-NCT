c65a986077c219261ca18c748c9b1d04241d7fe3
AliErsoy <aliersoytr35@gmail.com>
2023-01-18 23:25:16 +0300

    fc

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -5,14 +5,14 @@ const sidebar_margin = document.getElementById("mySidebar");
   if(getComputedStyle(sidebar_margin).width == "50px")
 {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("content").style.marginLeft = "250px";
+  //document.getElementById("content").style.marginLeft = "250px";
   document.getElementById("navbar").style.marginLeft = "250px";
   document.getElementById("dashboard-content").style.marginLeft = "0px";
 }
 
   if(getComputedStyle(sidebar_margin).width == "250px"){
   document.getElementById("mySidebar").style.width = "50px";
-  document.getElementById("content").style.marginLeft= "50px";
+  //document.getElementById("content").style.marginLeft= "50px";
   document.getElementById("navbar").style.marginLeft = "50px";
   document.getElementById("dashboard-content").style.marginLeft = "-500px";
 }
