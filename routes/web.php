60c0b1c0700323ef688fe642a5924495d63e02ea
AliErsoy <aliersoytr35@gmail.com>
2023-01-18 14:38:23 +0300

    getting it working again

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,6 @@ Route::post("product-register", "ProductRegister@Register");
 Route::get("admin-product-register", "ProductRegister@adminRegisterView")->name("admin-product-register");
 Route::post("admin-product-register", "ProductRegister@adminRegister");
 
-//Route::view('/hakkımızda', 'about-us')-name("about-us");
-//Route::view('/iletişim', 'contact-us')-name("route-name");
-//Route::view('/servislerimiz', 'services')-name("route-name");
+Route::view('/hakkımızda', 'about-us')-name("about-us");
+Route::view('/iletişim', 'contact-us')-name("route-name");
+Route::view('/servislerimiz', 'services')-name("route-name");
