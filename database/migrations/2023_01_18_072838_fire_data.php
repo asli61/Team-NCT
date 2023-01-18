bf70ac49ec4848a3a74270dce79b617a7752bdcb
asim61 <asimersoy2005@gmail.com>
2023-01-18 09:52:29 +0300

    migrations looks like they work, final adjustments

--- database/migrations/2023_01_18_072838_fire_data.php
+++ database/migrations/2023_01_18_072838_fire_data.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('FireTable', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')
-            ->references('id')->on('product_infos')->onDelete('cascade');
+            ->references('id')->on('product_infos')->onDelete('cascade')->nullable();
             $table->timestamps();
         });
     }
