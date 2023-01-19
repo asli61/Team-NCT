677c9f45b167b01eb6da94cb9077fd7e68de0196
asim61 <asimersoy2005@gmail.com>
2023-01-19 23:04:02 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -20,15 +20,12 @@ class FireDisplayController extends Controller
         $fireTable = FireTableInput::with("productInfo")->orderBy("created_at","desc")->take(1000)->get();
 
         $fireArray = [];
-        $adressArray = [];
 
         foreach($fireTable as $fires)
         {
             if($fires["created_at"] > now()->subHours(2))
             {
-                //$fireArray[] = "product_id" => $fires->productInfo->adress;
                 $fireArray[] = $fires;
-                $adressArray[] = [$fires["id"] => $fires->productInfo->adress];
             }     
 
             else
