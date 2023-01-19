7276b0670d79d4c4e0b7396cf7b5ba0e2d69e792
asim61 <asimersoy2005@gmail.com>
2023-01-19 21:54:57 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -21,7 +21,7 @@ class FireDisplayController extends Controller
 
         foreach($fireTable as $fires)
         {
-            if($fires["created_at"] > now()->subHours(20))
+            if($fires["created_at"] > now()->subHours(2))
             {
                 $fireArray[] = $fires;
             }     
