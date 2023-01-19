1ff2f8f984a31be79c833e9398fe8662d1e39aa6
asim61 <asimersoy2005@gmail.com>
2023-01-19 18:49:22 +0300

    fc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -12,7 +12,7 @@ class FireDisplayController extends Controller
     {
         
         $var = ProductInfo::with("product_id");//go on here
-        $fireTable = FireTableInput::orderBy("id","desc")->take(30)->get();
+        $fireTable = FireTableInput::orderBy("id","desc")->take(40)->get();
         $fireArray = [];
 
         foreach($fireTable as $fires)
