addaff9550af49cfefeebe29227c7aefab1e46d4
AliErsoy <aliersoytr35@gmail.com>
2023-01-19 14:26:33 +0300

    fc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,6 +59,26 @@
     </form>
     </div>
 </div>
+<div id="mySidebar" class="sidebar">
+    <button class="openbtn" onclick="openNav()">☰</button>  
+    <div id ="dashboard-content">
+        <a class= "navbar-link" href="{{route('home')}}">Yangın Bilgilendirme</a>
+        @guest
+            <a href="{{route('product-register')}}" class="navbar-link auth-link">Ürününüzü kayıt edin</a>
+            <a href="{{route('login')}}" class="navbar-link auth-link">Yönetici Giriş</a>
+        @endguest
+        @auth
+        <form method = "post" action="{{route('logout')}}">
+        @csrf
+        <button class = "log-out-button,navbar-link auth-link" type = "submit">Log Out</button>
+        </form>
+        <a class="navbar-link auth-link" href="{{route("admin-product-register")}}">Yönetici Ürün Ekleme</a>
+        @endauth
+        <a href="{{route("about-us")}}">Hakkımızda</a>
+        <a href="{{route("services")}}">Sağladığımız servisler</a>
+        <a href="{{route("contact-us")}}">Bize ulaşın</a>
+    </div>
+</div> 
 </body>
 </html>
 
