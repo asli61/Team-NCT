3ab9b3f03e80de5d15fa47044b846586fc20739c
asim61 <asimersoy2005@gmail.com>
2023-01-19 19:26:37 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -13,7 +13,7 @@ class FireDisplayController extends Controller
     {
         
         $var = ProductInfo::with("product_id");//go on here
-        $fireTable = FireTableInput::orderBy("created_at","desc")->take(1000)->get();
+        $fireTable = FireTableInput::orderBy("created_at","desc")->take(100)->get();
         //$fireTime = Carbon::parse($fireTable["created_at"]);
         $fireArray = [];
 
