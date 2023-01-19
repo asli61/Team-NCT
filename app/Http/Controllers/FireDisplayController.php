ccec25072bc6a971f045a32d3c0da02ba35e9825
asim61 <asimersoy2005@gmail.com>
2023-01-19 16:24:44 +0300

    code typo fix

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -9,7 +9,7 @@ class FireDisplayController extends Controller
 {
     public function index()
     {
-        $fireTable = FireTableInput::orederBy("id","desc")->take(50)->get();
+        $fireTable = FireTableInput::orderBy("id","desc")->take(50)->get();
         $fireArray = [];
 
         foreach($fireTable as $fires)
