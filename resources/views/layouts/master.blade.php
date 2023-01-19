59388b5e26603789e86de304df1ec471f525a393
AliErsoy <aliersoytr35@gmail.com>
2023-01-19 12:44:02 +0300

    fc

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -39,6 +39,13 @@
             <a href="{{route('product-register')}}" class="navbar-link auth-link">Ürününüzü kayıt edin</a>
             <a href="{{route('login')}}" class="navbar-link auth-link">Yönetici Giriş</a>
         @endguest
+        @auth
+        <form method = "post" action="{{route('logout')}}">
+        @csrf
+        <button class = "navbar-link auth-link" type = "submit">Log Out</button>
+        </form>
+        <a class="navbar-link auth-link" href="{{route("admin-product-register")}}">Yönetici Ürün Ekleme</a>
+        @endauth
         <a href="{{route("about-us")}}">Hakkımızda</a>
         <a href="{{route("services")}}">Sağladığımız servisler</a>
         <a href="{{route("contact-us")}}">Bize ulaşın</a>
