60c423b11049224e64e128109b6c8d9d661ca8c5
asim61 <asimersoy2005@gmail.com>
2023-01-19 19:45:28 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -23,8 +23,10 @@ class FireDisplayController extends Controller
             {
                 $fireArray[] = $fires;
             }
+            else
+            {
+                return view("fire-display")->with("fires", $fireArray);
+            }
         }
-
-        return view("fire-display")->with("fires", $fireArray);
     }
 }
