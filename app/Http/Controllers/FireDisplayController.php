ec07cb6a273f60b9a3f6ccad5573dd6a16fd26e6
asim61 <asimersoy2005@gmail.com>
2023-01-19 22:53:31 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -26,6 +26,7 @@ class FireDisplayController extends Controller
             if($fires["created_at"] > now()->subHours(2))
             {
                 $fireArray[] = $fires->productInfo->adress;
+                $fireArray[] = $fires;
             }     
 
             else
