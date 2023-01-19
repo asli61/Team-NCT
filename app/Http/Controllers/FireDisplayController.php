0f0e06c0934543b348ebb8cd4f7dd18e52e49501
asim61 <asimersoy2005@gmail.com>
2023-01-19 21:44:55 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -23,11 +23,8 @@ class FireDisplayController extends Controller
             if($fires["created_at"] > now()->subHours(2))
             {
                 $fireArray[] = $fires;
-            }
-            
-            
-            return view("fire-display")->with("fires", $fireArray);
-            
+            }     
         }
+    return view("fire-display")->with("fires", $fireArray);
     }
 }
