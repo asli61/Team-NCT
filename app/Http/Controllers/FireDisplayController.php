b3948d6107e939e32dd479c45518977da11fd8a9
asim61 <asimersoy2005@gmail.com>
2023-01-19 21:47:15 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -11,7 +11,7 @@ class FireDisplayController extends Controller
 {
     public function index()
     {
-        $fireTable= FireTableInput::select('FireTable.created_at', 'serial_number')
+        $fireTable= FireTableInput::select('FireTable.created_at', 'serial_number', "adress")
 ->join('product_infos', 'product_id', '=', 'product_infos.id')
 ->get();
         //$fireTable = FireTableInput::orderBy("created_at","desc")->take(1000)->get();//go on here
