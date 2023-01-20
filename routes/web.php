f6f104ee17bf62f18a9f5e95dd77e4fd749866e7
AliErsoy <aliersoytr35@gmail.com>
2023-01-20 21:44:40 +0300

    cc

--- routes/web.php
+++ routes/web.php
@@ -50,4 +50,4 @@ Route::get("fires", "FireDisplayController@index")->name("fire-display");
 Route::view('/hakkımızda', 'about-us')->name("about-us");
 Route::view('/servislerimiz', 'services')->name("services");
 Route::view('/iletişim', 'contact-us')->name("contact-us");
-Route::view('/yanginlar', 'fires')->name("fire-display");
+Route::view('/yanginlar', 'fires')->name("fires");
