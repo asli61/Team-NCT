e0a0f3cf95705b2adf355572dfd72d16e97b89dc
AliErsoy <aliersoytr35@gmail.com>
2023-01-20 19:11:28 +0300

    fc

--- resources/views/fire-display.blade.php
+++ resources/views/fire-display.blade.php
@@ -24,7 +24,7 @@ $array_key = 0;
 @endphp
 @foreach ($fires as $fire)
 
-<div>
+<div class="fire-contianer-styling">
      <div class="time-styling"> Oluşturulma zamanı: {{$fire["created_at"]->addHours(3)}} </div> 
      <div class="adress-styling"> Adres: {{$adress[$array_key]}} </div> 
      <div class="serial-styling"> Seri Numarası: {{$serial[$array_key]}} </div> 
