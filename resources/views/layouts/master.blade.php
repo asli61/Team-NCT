a87d0832dbf04d7c51f10cbf31e532187f935787
AliErsoy <aliersoytr35@gmail.com>
2023-01-20 21:57:33 +0300

    fc

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,7 +16,7 @@
     <div class = "navbar" id="navbar">
         <a class= "navbar-link" href="{{route('home')}}">Yangın Bilgilendirme</a>
         <a class= "navbar-link" href="{{route('home')}}">template</a>
-        <a href="/fires">yangınlar</a>
+        <a class="navbar-link" href="/fires">yangınlar</a>
 
         @guest
         <a href="{{route('product-register')}}" class="navbar-link auth-link">Ürününüzü kayıt edin</a>
