e170e98540e133bf33f55097348f90f35e8c4313
asim61 <asimersoy2005@gmail.com>
2023-01-30 02:32:38 +0300

    cc

--- app/Http/Controllers/FireDisplayController.php
+++ app/Http/Controllers/FireDisplayController.php
@@ -45,5 +45,6 @@ class FireDisplayController extends Controller
             }
         }
         //return view("fire-display")->with("fires", $fireArray);
+        return redirect()->back();
     }
 }
